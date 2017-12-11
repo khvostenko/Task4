f1cc72b5354db728641fd7af6916dc144b7b5871
khvostenko <34243982+khvostenko@users.noreply.github.com>
2017-12-11 15:29:18 +0200

    Add files via upload

---
+++ Task4/Task4/Matrix.cs
@@ -0,0 +1,245 @@
+﻿using System;
+
+namespace Task4
+{
+    class Matrix
+    {
+        private int countRow;
+        private int countColumn;
+        private double[,] _matrix;
+
+        public Matrix()
+        {
+            countRow = 0;
+            countColumn = 0;
+            _matrix = null;
+        }
+
+        public Matrix(int countRow, int countColumn)
+        {
+            this.countRow = countRow;
+            this.countColumn = countColumn;
+            _matrix = new double[countRow, countColumn];
+            
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                if ((i < countRow && i >= 0) && (j < countColumn && j >= 0))
+                {
+                    return _matrix[i, j];
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            set
+            {
+                if ((i < countRow && i >= 0) && (j < countColumn && j >= 0))
+                {
+                    _matrix[i, j] = value;
+                }
+                else
+                {
+                    _matrix[i, j] = 0;
+                }
+            }
+        }
+
+        public int GetRow
+        {
+            get
+            {
+                return countRow;
+            }
+            set
+            {
+                if(countRow>0)
+                {
+                    countRow = value;
+                }
+                else
+                {
+                    countRow = 0;
+                }
+            }
+        }
+
+        public int GetColumn
+        {
+            get
+            {
+                return countColumn;
+            }
+            set
+            {
+                if (countColumn > 0)
+                {
+                    countColumn = value;
+                }
+                else
+                {
+                    countColumn = 0;
+                }
+            }
+        }
+
+        public void DisplayMatrix()
+        {
+            for (int i = 0; i < countRow; i++)
+            {
+                for (int j = 0; j < countColumn; j++)
+                {
+                    Console.Write(_matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            if (A.countRow == B.countRow && A.countColumn == B.countColumn)
+            {
+                Matrix C = new Matrix(A.GetRow, A.GetColumn);
+                for (int i = 0; i < A.countRow; i++)
+                {
+                    for (int j = 0; j < A.countColumn; j++)
+                    {
+                        C[i, j] = A[i, j] + B[i, j];
+                    }
+                }
+
+                return C;
+            }
+
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            if (A.countRow == B.countRow && A.countColumn == B.countColumn)
+            {
+                Matrix C = new Matrix(A.GetRow, A.GetColumn);
+                for (int i = 0; i < A.countRow; i++)
+                {
+                    for (int j = 0; j < A.countColumn; j++)
+                    {
+                        C._matrix[i, j] = A._matrix[i, j] - B._matrix[i, j];
+                    }
+                }
+                return C;
+            }
+
+            else
+            {
+                throw new InvalidOperationException();
+            } 
+        }
+
+        public static Matrix operator *(Matrix A, double scalar)
+        {
+            Matrix C = new Matrix(A.GetRow, A.GetColumn);
+
+            for (int i = 0; i < A.countRow; i++)
+            {
+                for (int j = 0; j < A.countColumn; j++)
+                {
+                    C._matrix[i, j] = A._matrix[i, j] * scalar;
+                }
+            }
+            return C;
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            if (A.countRow == B.countColumn && A.countColumn == B.countRow) 
+            {
+                Matrix C = new Matrix(A.GetRow, A.GetRow);
+
+                for (int i = 0; i < A.countRow; i++)
+                {
+                    for (int j = 0; j < A.countRow; j++)
+                    {
+                        for (int h = 0; h < A.countColumn; h++)
+                        {
+                            C[i, j] += A[i, h] * B[h, j];
+                        }
+                    }
+                }
+
+                return C;
+            }
+            
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static Matrix Transponate(Matrix A)
+        {
+            Matrix C = new Matrix(A.GetColumn, A.GetRow);
+
+            for (int i = 0; i < A.countRow; i++)
+            {
+                for (int j = 0; j < A.countColumn; j++)
+                {
+                    C._matrix[j, i] = A._matrix[i, j];
+                }
+            }
+            return C;
+        }
+
+        public static bool Equal(Matrix A, Matrix B)
+        {
+            if (A.countRow != B.countRow || A.countColumn != B.countColumn)
+            {
+                return false;
+            }
+
+            else
+            {
+                for (int i = 0; i < A.countRow; i++)
+                {
+                    for (int j = 0; j < A.countColumn; j++)
+                    {
+                        if (A._matrix[i, j] != B._matrix[i, j]) 
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static Matrix CreateSubMatrix(Matrix A,int numberRow, int numberColumn)
+        {
+            if(numberRow<A.countRow && numberColumn<A.countColumn)
+            {
+                Matrix C = new Matrix(numberRow, numberColumn);
+
+                for(int i=0;i<numberRow;i++)
+                {
+                    for(int j=0;j<numberColumn;j++)
+                    {
+                        C[i, j] = A[i, j];
+                    }
+                }
+                return C;
+            }
+
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double scalarMatrixA;
+            double scalarMatrixB;
+
+            int subMatrixCountRowA;
+            int subMatrixCountColumnA;
+
+            int subMatrixCountRowB;
+            int subMatrixCountColumnB;
+
+            Matrix A = ReadFromFile("Matrix1.txt", out scalarMatrixA, out subMatrixCountRowA, out subMatrixCountColumnA);
+            Matrix B = ReadFromFile("Matrix2.txt", out scalarMatrixB, out subMatrixCountRowB, out subMatrixCountColumnB);
+
+            Console.WriteLine("Matrix A:");
+            A.DisplayMatrix();
+
+            Console.WriteLine("Matrix B:");
+            B.DisplayMatrix();
+
+            Console.WriteLine("Matrix A+B:");
+            try
+            {
+                (A + B).DisplayMatrix();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Matrix A-B:");
+            try
+            {
+                (A - B).DisplayMatrix();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Matrix A*B:");
+            try
+            {
+                (A * B).DisplayMatrix();
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Matrix comparison:");
+            Console.WriteLine(Matrix.Equal(A,B));
+
+            Console.WriteLine("Transponate matrix A: ");
+            Matrix.Transponate(A).DisplayMatrix();
+
+            Console.WriteLine("Transponate matrix B: ");
+            Matrix.Transponate(B).DisplayMatrix();
+
+            Console.WriteLine("Matrix A*scalar({0}): ", scalarMatrixA);
+            (A * scalarMatrixA).DisplayMatrix();
+
+            Console.WriteLine("Sub-Matrix A {0}x{1}:", subMatrixCountRowA, subMatrixCountColumnA);
+            Matrix.CreateSubMatrix(A, subMatrixCountRowA, subMatrixCountColumnA).DisplayMatrix();
+            Console.ReadLine();
+        }
+
+        public static Matrix ReadFromFile(string path, out double scalar, out int subMatrixCountRow, out int subMatrixCountColumn)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int countRow = Convert.ToInt32(sr.ReadLine());
+                int countColumn = Convert.ToInt32(sr.ReadLine());
+
+                Matrix A = new Matrix(countRow, countColumn);
+
+                for (int i = 0; i < countRow; i++)
+                {
+                    string[] text = sr.ReadLine().Split(' ');
+                    for (int j = 0; j < countColumn; j++)
+                    {
+                        A[i, j] = double.Parse(text[j]);
+                    }
+                }
+
+                scalar = double.Parse(sr.ReadLine());
+                subMatrixCountRow = int.Parse(sr.ReadLine());
+                subMatrixCountColumn = int.Parse(sr.ReadLine());
+
+                return A;
+            }
+        }
+    }
+}
